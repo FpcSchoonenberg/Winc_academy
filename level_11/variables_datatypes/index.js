adf4b7685e5f1949453086747a80b7445a727178
FpcSchoonenberg <76483668+FpcSchoonenberg@users.noreply.github.com>
2021-03-04 11:06:04 +0100

    Add files via upload

---
+++ level_11/variables_datatypes/index.js
@@ -0,0 +1,17 @@
+console.log('Hello')
+let naam = 'frank';
+console.log(naam);
+
+let getal1 = 4;
+let getal2 = 13;
+let som = getal1 + getal2;
+let onbekend = undefined;
+console.log(4 + 12);
+console.log(som);
+console.log(13 + onbekend);
+console.log(typeof 'ldkfj');
+
+
+
+
+
