c01cc3f2bf5a404b8fcca85182d21f13ba5b768f
FpcSchoonenberg <76483668+FpcSchoonenberg@users.noreply.github.com>
2021-03-05 13:20:35 +0100

    Add files via upload

---
+++ level_13/index.js
@@ -0,0 +1,24 @@
+// const age = 18;
+/* const isFemale = false;
+
+if (isFemale ==true) {
+    console.log("you walk like a woman, but talk like a man.");
+}
+else {
+    console.log("ladies only!!");
+} */
+/* if (age => 18) {
+    console.log("you are old enough,come in and have fun.");
+}
+else {
+    console.log("you are to young, you'd better go home and drink a cup of chocolate milk with your mother.");
+}
+ */
+
+const driverStatus = 'bob';
+if (driverStatus == 'blob') {
+    console.log('you got a license to drive')
+}
+else {
+    console.log('where is bob');
+}
